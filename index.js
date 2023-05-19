00909ca4f93a3ac79caf31c3349f487fd51e487d
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-05-19 17:34:55 -0300

    Desafio 2 concluido

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// DESAFIO 1
 let favoriteFood = 'Temaki'
 document.getElementById('text').innerHTML = favoriteFood
 
 
+// DESAFIO 2 //
+let productValue = Number(prompt('Enter the product value: '))
+
+if (productValue >= 20) {
+    document.getElementById('result').innerHTML = 'Approved'
+}
+else {
+    document.getElementById('result').innerHTML = 'Denied'
+}
+/* console.log(typeof(productValue)) */
 
